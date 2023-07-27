15624cc6be980de711ecb55951ebc299877bf356
ahad1033 <a.ahad2232@gmail.com>
2023-07-27 21:41:51 +0600

    Task 4 Solved

--- index.js
+++ index.js
@@ -23,3 +23,49 @@ const sumOfPositiveNumbers = (arr) => {
 
     return sum;
 };
+
+// Task 3: Write a JavaScript program to find the most frequent element in an array and return it. 
+
+const findMostFrequentElement = (arr) => {
+    if (arr.length === 0) {
+      return undefined;
+    }
+  
+    const frequencyMap = arr.reduce((map, element) => {
+      map[element] = (map[element] || 0) + 1;
+      return map;
+    }, {});
+  
+    let maxFrequency = 0;
+    let mostFrequentElement;
+  
+    for (const element in frequencyMap) {
+      if (frequencyMap[element] > maxFrequency) {
+        maxFrequency = frequencyMap[element];
+        mostFrequentElement = element;
+      }
+    }
+  
+    return mostFrequentElement;
+  };  
+
+//   Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+
+const findTwoNumbers = (sortedArr, target) => {
+    let left = 0;
+    let right = sortedArr.length - 1;
+  
+    while (left < right) {
+      const sum = sortedArr[left] + sortedArr[right];
+  
+      if (sum === target) {
+        return [left, right];
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  
+    return [];
+  };
