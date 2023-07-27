89a54eee3da746fcaa2ee2fc85d521219af4fd0e
ahad1033 <a.ahad2232@gmail.com>
2023-07-27 21:29:07 +0600

    Task 2 Solved

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const reverseString = (str) => {
 
     return reversedString;
 };
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+
+const sumOfPositiveNumbers = (arr) => {
+    let sum = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 0){
+            sum += arr[i];
+        }
+    }
+
+    return sum;
+};
